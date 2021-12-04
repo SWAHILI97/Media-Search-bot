06ca6d05c09a353f82c84a72a9df1b59f525b010
hramamohamed <53920453+hramamohamed@users.noreply.github.com>
2021-12-04 21:42:56 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -197,3 +197,22 @@ async def bot_info(bot, message):
         ]
         ]
     await message.reply(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+@Client.on_message(filters.command('addposter') & filters.user(ADMINS))
+async def add_poster(bot, message):
+    """Media Handler"""
+    reply = message.reply_to_message
+    if reply and reply.media:
+        msg = await message.reply("Processing...⏳", quote=True)
+    else:
+        await message.reply('Reply to file with /addposter which you want to delete', quote=True)
+        return
+    for file_type in ("document", "video", "audio"):
+        media = getattr(reply, file_type, None)
+        if media is not None:
+            break
+    else:
+        return
+
+    media.file_type = await reply.ask(text=" send description")
+    media.caption = message.caption
+    await save_file(media
