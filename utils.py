f17d5f67f957b32ffca43b56c96a0c89d245ea23
hramamohamed <53920453+hramamohamed@users.noreply.github.com>
2021-12-05 13:04:18 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -35,6 +35,7 @@ class Media(Document):
     file_size = fields.IntField(required=True)
     file_type = fields.StrField(allow_none=True)
     mime_type = fields.StrField(allow_none=True)
+    description = fields.StrField(allow_none=True)
     caption = fields.StrField(allow_none=True)
 
     class Meta:
@@ -68,7 +69,7 @@ async def save_poster(imdb_id, title, year, url):
         else:
             logger.info("Poster is saved in database")
 
-async def save_file(media):
+async def save_file(media,description):
     """Save file in database"""
 
     # TODO: Find better way to get same file_id for same media to avoid duplicates
@@ -82,6 +83,7 @@ async def save_file(media):
             file_size=media.file_size,
             file_type=media.file_type,
             mime_type=media.mime_type,
+            description=description.html,
             caption=media.caption.html if media.caption else None,
         )
     except ValidationError:
