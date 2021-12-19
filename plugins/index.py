0dabd67406579682500bb69d2296f0fa344108f8
hramamohamed <53920453+hramamohamed@users.noreply.github.com>
2021-12-19 16:20:19 +0300

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -1,80 +1,24 @@
-import logging
-import asyncio
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
-from info import ADMINS
-import os
-from utils import save_file
-import pyromod.listen
-logger = logging.getLogger(__name__)
-lock = asyncio.Lock()
+import datetime
+from info import DATABASE_URI, DATABASE_NAME, CHANNELS
+from plugins.channel import Database
+
+db = Database(DATABASE_URI, DATABASE_NAME)
 
 
-@Client.on_message(filters.command(['index', 'indexfiles']) & filters.user(ADMINS))
-async def index_files(bot, message):
-    """Save channel or group files"""
-    if lock.locked():
-        await message.reply('Wait until previous process complete.')
-    else:
-        while True:
-            last_msg = await bot.ask(text = "Forward me last message of a channel which I should save to my database.\n\nYou can forward posts from any public channel, but for private channels bot should be an admin in the channel.\n\nMake sure to forward with quotes (Not as a copy)", chat_id = message.from_user.id)
-            try:
-                last_msg_id = last_msg.forward_from_message_id
-                if last_msg.forward_from_chat.username:
-                    chat_id = last_msg.forward_from_chat.username
-                else:
-                    chat_id=last_msg.forward_from_chat.id
-                await bot.get_messages(chat_id, last_msg_id)
-                break
-            except Exception as e:
-                await last_msg.reply_text(f"This Is An Invalid Message, Either the channel is private and bot is not an admin in the forwarded chat, or you forwarded message as copy.\nError caused Due to <code>{e}</code>")
-                continue
+async def handle_user_status(bot, cmd):
+    chat_id = cmd.from_user.id
+    if not await db.is_user_exist(chat_id):
+        await db.add_user(chat_id)
+        await bot.send_message(
+            CHANNELS,
+            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started!!"
+        )
 
-        msg = await message.reply('Processing...⏳')
-        total_files = 0
-        async with lock:
-            try:
-                total=last_msg_id + 1
-                current=int(os.environ.get("SKIP", 2))
-                nyav=0
-                while True:
-                    try:
-                        message = await bot.get_messages(chat_id=chat_id, message_ids=current, replies=0)
-                    except FloodWait as e:
-                        await asyncio.sleep(e.x)
-                        message = await bot.get_messages(
-                            chat_id,
-                            current,
-                            replies=0
-                            )
-                    except Exception as e:
-                        print(e)
-                        pass
-                    try:
-                        for file_type in ("document", "video", "audio"):
-                            media = getattr(message, file_type, None)
-                            if media is not None:
-                                break
-                            else:
-                                continue
-                        media.file_type = file_type
-                        media.caption = message.caption
-                        await save_file(media)
-                        total_files += 1
-                    except Exception as e:
-                        print(e)
-                        pass
-                    current+=1
-                    nyav+=1
-                    if nyav == 20:
-                        await msg.edit(f"Total messages fetched: {current}\nTotal messages saved: {total_files}")
-                        nyav -= 20
-                    if current == total:
-                        break
-                    else:
-                        continue
-            except Exception as e:
-                logger.exception(e)
-                await msg.edit(f'Error: {e}')
-            else:
-                await msg.edit(f'Total {total_files} Saved To DataBase!')
+    ban_status = await db.get_ban_status(chat_id)
+    if ban_status["is_banned"]:
+        if (
+                datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
+        ).days > ban_status["ban_duration"]:
+            await db.remove_ban(chat_id)
+       
+    await cmd.continue_propagation()
